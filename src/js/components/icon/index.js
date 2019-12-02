e8022790f78d8360d1ee5676e20cd1b9d9f25655
Nima Omrani <nima.omrani1988@gmail.com>
2019-12-02 16:16:22 +0330

    udpate: icon component

--- src/js/components/icon/index.js
+++ src/js/components/icon/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './styles';
 
-export default props => {
+export default ({ id, url, viewBox, className }) => {
 	/**
-	 * remove '-usage' keyword if present from id 
+	 * remove '-usage' keyword if present from id
 	 * svg-sprite-loader adds '-usage' keyword to generated id in extracted mode
-	 * for more info check https://github.com/kisenka/svg-sprite-loader/issues/123	 
+	 * for more info check https://github.com/kisenka/svg-sprite-loader/issues/123
 	 */
-	const usageIndex = props.id.indexOf('-usage');
-	const className = usageIndex < 0 ? props.id : props.id.substr(0, usageIndex);
-	
+	const usageIndex = id.indexOf('-usage');
+	const iconClass = usageIndex < 0 ? id : id.substr(0, usageIndex);
+
 	return (
-		<svg className={styles[className]} viewBox={props.viewBox}>
-			<use xlinkHref={props.url}/>
+		<svg className={classNames(styles.icon, styles[iconClass], className)} viewBox={viewBox}>
+			<use xlinkHref={`#${id}`}/>
 		</svg>
 	);
-}
+};
